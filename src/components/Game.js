6e1e3c22b27dd9dee98ee3339d49f7f076752387
M. Pennella <michael.pennella@gmail.com>
2019-01-10 03:51:00 -0800

    Created component for main game area

---
+++ src/components/Game.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Game extends Component {
+  state = {
+  };
+
+  
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default Game;
