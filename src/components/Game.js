4a7a61096309969ee72dad76f06c941f6c9a5617
M. Pennella <michael.pennella@gmail.com>
2019-01-10 15:19:13 -0800

    Page now renders all signs

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,18 +2,36 @@ import React, { Component } from "react";
 import GamePiece from "./GamePiece";
 
 class Game extends Component {
-  state = {
-    score: 0
-  };
+    state = {
+        score: 0
+    };
 
+    zodiacSigns = [
+        "Aries",
+        "Taurus",
+        "Gemini",
+        "Cancer",
+        "Leo",
+        "Virgo",
+        "Libra",
+        "Scorpio",
+        "Sagittarius",
+        "Capricorn",
+        "Aquarius",
+        "Pisces"
+    ]
+
+    renderPieces() {
+        return this.zodiacSigns.map( sign => <GamePiece name={sign} />)
+    }
   
-  render() {
-    return (
-      <div>
-        <GamePiece name="Taurus" />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                {this.renderPieces()}
+            </div>
+        );
+    }
 }
 
 export default Game;
--- src/components/GamePiece.js
+++ src/components/GamePiece.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 function GamePiece(props) {
-  return (
-    <div>
-        <img src={require(`../assets/${props.name}.jpg`)} alt={props.name} />
-    </div>
-  );
+    return (
+        <div>
+            <img src={require(`../assets/${props.name}.jpg`)} alt={props.name} />
+        </div>
+    );
 }
 
 export default GamePiece;
